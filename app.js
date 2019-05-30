6eff28c62a1f7a66fff5ba4d15bea75496190fa5
MuktiPaudel <muktinarayan.paudel@gmail.com>
2019-05-30 18:22:16 +0300

    Mongoose Schema Error: Validation error-Cast to String failed for value

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true });
+//Get the default connection
+let db = mongoose.connection;
+
+// check the connection
+db.once('open', function(){
+    console.log('connected to database');
+});
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Init the app
+const app = express();
+
+//Bring in the models
+let Article = require('./models/article');
+
+//load the view engine
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// body parser middleware, parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/json
+app.use(bodyParser.json())
+
+// index.pug route
+app.get('/', (req, res) => {
+
+  Article.find({}, function (err, articles){
+      if(err){
+          console.log(err);
+      } else {
+        res.render('index', {
+            title:'articles',
+            articles : articles
+        });
+      }
+  }); 
+});
+
+// add route
+app.get('/articles/add', (req, res) =>{
+    res.render('add_article',{
+        title: 'add articles',
+    });
+  });
+
+// add form submit post route
+app.post('/articles/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+     if(err){
+         console.log(err);
+         return;
+     }else{
+         res.redirect('/');
+     }
+    });
+  });
+
+
+
+app.listen(3000, () => console.log(`App running on port 3000`));
